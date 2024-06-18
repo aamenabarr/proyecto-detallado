86b0320316683e0d4f2cb294a60f6983b1494ace
aamenabarr <aamenabarr@uc.cl>
2024-06-18 11:08:18 -0400

    First commit E3

--- Fire-Emblem/Fire-Emblem-Controller/obj/Debug/net8.0/Fire-Emblem-Controller.AssemblyInfo.cs
+++ Fire-Emblem/Fire-Emblem-Controller/obj/Debug/net8.0/Fire-Emblem-Controller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daba9511ae0dc4714d6eb671abea98a216d36922")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fire-Emblem-Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
